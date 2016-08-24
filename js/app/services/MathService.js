696201bcef5a94118083962044fae9aeb3078dc3
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-24 18:00:52 -0400

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,9 @@
-// Your code here!
+function MathService () {
+  this.sum = function (array) {
+    return array.reduce(function(a, b) {return a + b;}, 0);
+  }
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService)
