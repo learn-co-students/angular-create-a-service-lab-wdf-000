8c2c4c514e393cef67bac5275f241f5f15f93f41
eutopian <eutopia@gmail.com>
2016-12-05 12:12:35 -0500

    Update MathService.js

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,13 +1,13 @@
 function MathService() {
-	this.sum = function(array) {
-		var total =	array.reduce(function(a, b) {
-			return a + b; 
-		}, 0);
-		return total;
+  this.sum = function(array) {
+    var total = array.reduce(function(a, b) {
+      return a + b; 
+    }, 0);
+    return total;
   }
 }
 
 angular
-	.module('app')
-	.service('MathService', MathService);
+  .module('app')
+  .service('MathService', MathService);
 	
